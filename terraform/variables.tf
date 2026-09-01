e8ff98045fe0c62e7c420158eac37a93012b5bb7
Payal V <vishwpay@gmail.com>
2026-09-01 19:48:09 +0530

    updated the code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,15 +30,15 @@ variable "node_instance_type" {
 
 variable "node_desired_count" {
   type    = number
-  default = 2
+  default = 3
 }
 
 variable "node_min_count" {
   type    = number
-  default = 2
+  default = 3
 }
 
 variable "node_max_count" {
   type    = number
-  default = 2
+  default = 3
 }
